be612f81ac70d7d90251bdc4b25c6f2629aea889
JJ Cheng <jonathanchengjr77@gmail.com>
2021-11-10 13:07:57 -0500

    XBee communication working

--- data_hub/script.py
+++ data_hub/script.py
@@ -21,9 +21,10 @@ import schema_checks
 # globals -------------------------------------------------
 
 ERROR_STR = "ERROR00000000000"
-POLLING_INTERVAL = 0.1  # number of seconds between GPIO polls
+POLLING_INTERVAL = 0.5  # number of seconds between GPIO polls
 UPDATE_INTERVAL = 15    # number of seconds between backend updates
-NUM_MSG_PARTS = 4       # number of parts in chair-to-hub message
+NUM_MSG_PARTS = 3       # number of parts in chair-to-hub message
+WORKSPACE_ID = 0
 
 # TODO: configure rpi for serial communication
 # See the circuitdigest article for instructions.
@@ -65,17 +66,28 @@ def read_serial():
 
     If a read error occurs (ie. due to bad setup), undefined behavior.
     '''
-    x = ser.readline().strip()
-    print(x)
-    parts = x.split(',')
-    if len(msg_parts) != NUM_MSG_PARTS: 
+    x = str(ser.readline(), 'UTF-8').strip()
+    print('Serial Read: ', x)
+    parts = x.split(', ')
+    if len(parts) != NUM_MSG_PARTS: 
+        print('Num Parts dont match: ', len(parts), 'vs', NUM_MSG_PARTS)
+        return None
+    status = False
+    if parts[1] == 'OCCUPIED':
+        status = True
+    elif parts[1] == 'EMPTY':
+        status = False
+    else:
+        print('Unknown Status ', parts[1])
         return None
     data = {
-        'workspace_id': parts[0],
-        'seat': parts[1],
-        'old': parts[2],
-        'new': parts[3],
+        #'workspace_id': parts[0],
+        'workspace_id': str(WORKSPACE_ID),
+        'seat': parts[0],
+        'old': not status,
+        'new': status,
     }
+    print('Serial Data:', data)
     return data
 
 
@@ -83,7 +95,8 @@ def get_time():
     '''
     Gets the current time from the time module and returns the int-cast.
     '''
-    return int(time.time())
+    seconds = int(time.time())
+    return seconds
 
 
 def format_state(state):
@@ -137,12 +150,13 @@ def update_state(state, data, my_id):
     old = data['old']
     new = data['new']
     seats = state['seats']
-    if workspace_id != my_id:
+    if workspace_id != str(my_id):
+        print("Mismatched workspace ids ", workspace_id, str(my_id))
         return state
     if seat in seats and seats[seat] != old:
         print("Warning: chair id " + seat +
               " had a differing status.")
-        print("RPi: " + seats[seat] + ", Chair: " + old)
+        print("RPi: " + str(seats[seat]) + ", Chair: " + str(old))
     elif seat not in seats:
         print("Note: Adding chair id " +
               seat + " to workspace state")
@@ -156,11 +170,12 @@ def update_state(state, data, my_id):
 
 def main():
     serial_res = get_serial_number()
-    WORKSPACE_ID = serial_res[0]
-    if (WORKSPACE_ID == ERROR_STR):
-        print("ERROR: could not get RPi serial number")
-        print(serial_res[1])
-        return
+    #WORKSPACE_ID = serial_res[0]
+    #if (WORKSPACE_ID == ERROR_STR):
+    #    print("ERROR: could not get RPi serial number")
+    #    print(serial_res[1])
+    #    return
+    print('WORKSPACE_ID ', WORKSPACE_ID)
 
     # url pointing to web app backend POST endpoint
     BACKEND = 'http://freeseats-a3.herokuapp.com/api/freeseats'
@@ -170,11 +185,12 @@ def main():
         '_id': WORKSPACE_ID,
         'seats': {}
     }
-    res = requests.post(BACKEND+'/create_hub', json=register_payload)
-    if res.status_code == 400:
-        print(res)
-        print(json.dumps(res.json(), indent=4))
-        raise Exception("Main script failed to register with backend", BACKEND+'/create_hub')
+    # TODO: Uncommment for full functionality
+    #res = requests.post(BACKEND+'/create_hub', json=register_payload)
+    #if res.status_code == 400:
+    #    print(res)
+    #    print(json.dumps(res.json(), indent=4))
+    #    raise Exception("Main script failed to register with backend", BACKEND+'/create_hub')
 
     # list of chair statuses
     state = {
@@ -192,14 +208,14 @@ def main():
         time.sleep(POLLING_INTERVAL)
         curr_time = get_time()
         if (curr_time >= last_update + UPDATE_INTERVAL):
-            send_state(state, BACKEND)
-            print(state)
+            # TODO: Uncomment for full functionality
+            #send_state(state, BACKEND)
+            print('State: ', state)
             last_update = curr_time
 
         data = read_serial()  # converted string to json data
-        if data == None:
-            continue
-        update_state(state, data, WORKSPACE_ID)
+        if data != None:
+            update_state(state, data, WORKSPACE_ID)
 
 
 # execute -------------------------------------------------
