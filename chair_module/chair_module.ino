d49fb21658c66cecc021cefb8fc7239641ef05ff
William Foy <wfoy@andrew.cmu.edu>
2021-11-17 12:11:53 -0500

    arduino code updated to get baseline, send on changes

--- chair_module/chair_module.ino
+++ chair_module/chair_module.ino
@@ -1,32 +1,35 @@
 #include <CapacitiveSensor.h>
 
 /*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
- * Uses a high value resistor e.g. 10 megohm between send pin and receive pin
- * Resistor effects sensitivity, experiment with values, 50 kilohm - 50 megohm. Larger resistor values yield larger sensor values.
- * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
- * Best results are obtained if sensor foil and wire is covered with an insulator such as paper or plastic sheet
+ * ECE Capstone Project - FreeSeats
+ * William Foy, Jonathan Cheng, Nathan Ang
+ * Fall 2021
  */
 
 
-CapacitiveSensor cs_4_2 = CapacitiveSensor(2,4);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
+CapacitiveSensor cs_4_2 = CapacitiveSensor(4,2);        // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
 long baseline_avg = 0;
+int occupied;
+int empty;
 
 void setup()                    
 {
-   pinMode(LED_BUILTIN, OUTPUT);
    cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
    long avg = 0;
    for(int i = 0; i < 20; i++) {
-    avg += cs_4_2.capacitiveSensor(30);
+    long tot = cs_4_2.capacitiveSensor(30);
+    Serial.print(tot);
+    Serial.print(" ");
+    avg += tot;
     delay(500);
    }
-   baseline_avg = avg/10;
+   baseline_avg = avg/20;
    char buf[50];
    sprintf(buf, "BASELINE = %lu\n", baseline_avg);
    Serial.print(buf);
+   occupied = 0;
+   empty = 0;
 }
 
 void loop()                    
@@ -35,16 +38,22 @@ void loop()
 
     //Serial.print(total1);                  // print sensor output 1
     //Serial.print("\n");
-    if (total1 > (baseline_avg * 1.2)) {
-        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        char buf[50];
-        sprintf(buf, "1, OCCUPIED, %lu\n", total1);
-        Serial.print(buf);
-    } else {
-        digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-        char buf[50];
-        sprintf(buf, "1, EMPTY, %lu\n", total1);
-        Serial.print(buf);
+    if (total1 > (baseline_avg * 10) && occupied != 4) {
+        occupied++;
+        if (occupied == 4) {
+          char buf[50];
+          sprintf(buf, "1, OCCUPIED, %lu\n", total1);
+          Serial.print(buf);
+          empty = 0;
+        }
+    } else if (total1 < (baseline_avg * 10) && empty != 4) {
+        empty++;
+        if (empty == 4) {
+          char buf[50];
+          sprintf(buf, "1, EMPTY, %lu\n", total1);
+          Serial.print(buf);
+          occupied = 0;
+        }
     }
     
 
