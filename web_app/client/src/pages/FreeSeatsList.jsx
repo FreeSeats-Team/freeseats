8a89c3e673a240fa8c8818ce49f887122a7f32d9
Sean Prendi <32982087+SeanPrendi@users.noreply.github.com>
2021-11-24 15:42:58 -0500

    Modal on location click (#4)
    * Refactored FreeSeatsList to be a functional component.
    
    * Migrated table to material UI.
    
    * Added functionality for mocking location suffixes.
    
    * Added modal on pressing space.
    
    Co-authored-by: Nathan Ang <nathanang2000@gmail.com>

--- web_app/client/src/pages/FreeSeatsList.jsx
+++ web_app/client/src/pages/FreeSeatsList.jsx
@@ -1,72 +1,115 @@
-import React, { useState, useEffect } from 'react'
-import ReactTable from 'react-table'
-import api from '../api'
-import styled from 'styled-components'
-import 'react-table/react-table.css'
+import React, { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
+
+// wrapper around api that adds location suffixes randomly
+import mockSuffixes from "../utils/MockSuffixes";
+import getLocationsBySpace from "../utils/GetLocationsBySpace";
+import FreeSeatsModal from "../components/FreeSeatsModal";
+
 
 const Wrapper = styled.div`
-    padding: 0 40px 40px 40px;
-`
+  padding: 0 40px 40px 40px;
+`;
 
 function FreeSeatsList() {
-    const [is_loading, set_is_loading] = useState(false);
-    const [freeseat_data, set_freeseat_data] = useState([]);
+  const [is_loading, set_is_loading] = useState(false);
+  const [freeseat_data, set_freeseat_data] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalSpace, setModalSpace] = useState("");
 
-    useEffect(() => {
-        set_is_loading(true);
-        
-        async function fetchData() {
-            await api.getAllFreeSeats().then(res => {
-                set_freeseat_data(res.data);
-                set_is_loading(false);
-            });
-        };
-        fetchData();
-    }, []);
-    
-    const columns = [
-        {
-            Header: 'Space',
-            accessor: '_id',
-            filterable: true,
-        },
-        {
-            Header: 'Free Seats',
-            accessor: 'seats',
-            Cell: function(props) {
-                console.log(Object.keys(props.original.seats).length)
-                return (
-                    <span>{Object.keys(props.original.seats).length}</span>
-                )
-            },
-        },
-    ]
-    
-    let showTable = true
-    if (!freeseat_data) {
-        showTable = false
-    }
-    if (!is_loading) {
-        console.log(freeseat_data.data)
-    }
-    else {
-        console.log("Loading")
-    }
+  useEffect(() => {
+    set_is_loading(true);
 
-    return (
-        <Wrapper>
-            {showTable && (
-                <ReactTable
-                    data={freeseat_data.data}
-                    columns={columns}
-                    loading={is_loading}
-                    defaultPageSize={10}
-                    showPageSizeOptions={true}
-                    minRows={0}
-                />
-            )}
-        </Wrapper>
-    )
+    async function fetchData() {
+      const locationData = await mockSuffixes();
+      set_freeseat_data(locationData);
+      set_is_loading(false);
+    }
+    fetchData();
+  }, []);
+
+  const createData = (space, num_seats) => ({
+    id: space,
+    space: (
+      <Button
+        variant="text"
+        style={{ textTransform: "none" }}
+        onClick={() => {
+          setModalSpace(space);
+          setModalOpen(true);
+        }}
+      >
+        {space}
+      </Button>
+    ),
+    num_seats,
+  });
+
+  let showTable = true;
+  if (!freeseat_data) {
+    showTable = false;
+  }
+  if (!is_loading) {
+    console.log(freeseat_data.data);
+  } else {
+    console.log("Loading");
+  }
+
+  let rows;
+  if (freeseat_data) {
+    rows = freeseat_data.map((x) =>
+      createData(x._id, Object.keys(x.seats).length)
+    );
+  } else {
+    rows = [];
+  }
+
+  const locationsBySpace = getLocationsBySpace(freeseat_data);
+  console.log(locationsBySpace);
+
+  return (
+    <div style={{ width: "100%" }}>
+      <Wrapper style={{ width: "80vw", margin: "auto" }}>
+        <FreeSeatsModal
+          data={locationsBySpace}
+          spaceName={modalSpace}
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+        />
+        {showTable && (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Space</TableCell>
+                  <TableCell align="right">Number of Seats</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{row.space}</TableCell>
+                    <TableCell align="right">{row.num_seats}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </Wrapper>
+    </div>
+  );
 }
 
-export default FreeSeatsList
+export default FreeSeatsList;
---
+++ web_app/client/src/utils/MockSuffixes.js
@@ -0,0 +1,38 @@
+import { getAllFreeSeats } from "../api";
+
+const generateLocation = () => {
+  const locations = ["tables", "lounge", "books"];
+  const randomLocation =
+    locations[Math.floor(Math.random() * locations.length)];
+  return randomLocation;
+};
+
+const mockSuffixes = async () => {
+  const res = await getAllFreeSeats();
+  const data = await res.data.data;
+
+  // Mapping data retrieved to add suffixes to the end randomly
+  // because currently data in the database does not have these suffixes
+  const locationData = data.map((x) =>
+    Object.fromEntries(
+      Object.entries(x).map(([k, v]) => {
+        if (k === "seats") {
+          return [
+            k,
+            Object.fromEntries(
+              Object.entries(v).map(([k1, v1]) => {
+                const newK = k1.concat("_", generateLocation());
+                return [newK, v1];
+              })
+            ),
+          ];
+        } else {
+          return [k, v];
+        }
+      })
+    )
+  );
+  return locationData;
+};
+
+export default mockSuffixes;
